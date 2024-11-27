c9158403f0a0596e295cf49c477112063f4d6508
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2024-11-27 10:41:54 +0000

    Entity key value

---
+++ CA2_BinaryTree_DianeDalyop/EntityKeyVal.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef KEYVALUEPAIR_H
+#define KEYVALUEPAIR_H
+
+template <typename K, typename V>
+class KeyValuePair {
+public:
+    K key;
+    V value;
+
+    // Constructor
+    KeyValuePair(K key, V value) : key(key), value(value) {}
+
+    // Overload equality operator for comparison (useful for TreeMap operations)
+    bool operator==(const KeyValuePair<K, V>& other) const {
+        return key == other.key && value == other.value;
+    }
+
+    // Overload less-than operator for sorting by key
+    bool operator<(const KeyValuePair<K, V>& other) const {
+        return key < other.key;
+    }
+
+    
+    void display() const {
+        std::cout << "{" << key << ": " << value << "}";
+    }
+};
+
+#endif // KEYVALUEPAIR_H
